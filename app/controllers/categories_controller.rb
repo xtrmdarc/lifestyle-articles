b4e1d69e22d0c6fd90677dbf1a42f1a86f994a42
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-21 12:40:18 -0500

    Add routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,5 @@
+class CategoriesController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  resources :categories, only: [:index, :show]
+  resources :articles, only: [:new, :create, :index, :show]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
+  root 'categories#index'
 end
