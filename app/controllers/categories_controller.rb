3cf46febb329cfaecfc76e38cfaa91329fdb8466
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-21 20:48:29 -0500

    Add most voted article feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
   def index
-    
+    @most_voted_article = Article.most_voted_article
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :votes
   
+  def self.most_voted_article
+    Article.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(1).first
+  end
 end
