7841bf6ba8d282e1168e579c29d5cd5674d3c78e
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-21 23:16:13 -0500

    Add signup page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:create]
   resources :categories, only: [:index, :show]
   resources :articles, only: [:new, :create, :index, :show]
+  get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
   root 'categories#index'
